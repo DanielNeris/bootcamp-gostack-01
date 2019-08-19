60c1a2ff67fafd8ff6ee453aa296d986463927d4
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-08-19 15:18:13 -0300

    Query & Route params

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const server = express();
 
-server.get('/test', (req, res) => {
-    return res.json({message: 'Hello World'});
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "daniel", "linkedin": "https://www.linkedin.com/in/danielneris/" }
+
+server.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.query;
+
+    return res.json({message: `Hello ${name} seu id é ${id}`});
 });
 
 server.listen(3333);
